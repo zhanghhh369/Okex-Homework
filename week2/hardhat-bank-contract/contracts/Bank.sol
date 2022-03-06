b990e5e6bd817ee602b072c0d9d25033f79a410b
zhanghhh369 <zhanghhh369@163.com>
2022-03-06 17:21:02 +0800

    第二周作业

--- week2/hardhat-bank-contract/contracts/Bank.sol
+++ week2/hardhat-bank-contract/contracts/Bank.sol
@@ -4,16 +4,20 @@ contract Bank{
     event logdata(uint x);
     uint total;
     mapping(address => uint) public balances;
+    address[] addrs;
 
     constructor() payable {
     }
 
-    // 这是一个回调函数, 收到以太币会被调用
+    // 收到以太币会被调用
     receive() external payable {
       emit logdata(msg.value);
+      //如果是第一次转钱，则初始化map
       if(balances[msg.sender]==0){
         update(msg.value);
+        addrs.push(msg.sender);
       }
+      //如果是多次转钱，则加钱
       else{
         deposit(msg.value);
       }
@@ -28,17 +32,32 @@ contract Bank{
         balances[msg.sender] += money;
     }
 
-    function saveTransferEth(address addr) public payable{
-        (bool success, ) = addr.call{value: total}("");
+    function saveTransferEth(address userAddr) public payable{
+        (bool success, ) = userAddr.call{value: total}("");
         require(success, 'TransferHelper::safeTransferETH: ETH transfer failed');
     }
 
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
+    function getBalance(address userAddr) public view returns (uint) {
+        return balances[userAddr];
     }
 
-    function withdrawAll(address addr) public {
-        saveTransferEth(addr);
+    function withdrawAll(address userAddr) public {
+        uint len = addrs.length;
+        for (uint i = 0; i < len; i++) {
+            balances[addrs[i]] = 0;
+            remove(i);
+        }
+        saveTransferEth(userAddr);
+    }
+
+    function remove(uint index) public {
+        uint len = addrs.length;
+        if (index == len - 1) {
+            addrs.pop();
+        } else {
+            addrs[index] = addrs[len - 1];
+            addrs.pop();
+        }
     }
 
 }
---
+++ week2/remix-week2/contracts/score.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.8.4;
+
+contract Teacher {
+    address public scoreAddress;
+
+    constructor() {
+        Score score = new Score(msg.sender);
+        scoreAddress = address(score);
+    }
+
+    function getGrade(address studentAddr) external view returns (uint) {
+        return IScore(scoreAddress).getGrade(studentAddr);
+    }
+
+    function setGrade(address studentAddr, uint grade) external {
+        IScore(scoreAddress).setGrade(studentAddr, grade);
+    }
+}
+
+interface IScore {
+    function getGrade(address studentAddr) external view returns (uint);
+    function setGrade(address studentAddr, uint grade) external;
+}
+
+
+contract Score {
+    address teacherAddr;
+    mapping(address => uint) public grades;
+    constructor(address teacherAddrNew){
+        teacherAddr = teacherAddrNew;
+    }
+    modifier onlyTeacher() {
+        require(msg.sender == teacherAddr, "Must be teacher");
+        _;
+    }
+    modifier lessThan100(uint grade) {
+        require(grade<100, "Scores are too large");
+        _;
+    }
+    function setGrade(address studentAddr, uint grade) public onlyTeacher lessThan100(grade) {
+        grades[studentAddr] = grade;
+    }
+    function getGrade(address studentAddr) public view returns (uint) {
+        return grades[studentAddr];
+    }
+}
